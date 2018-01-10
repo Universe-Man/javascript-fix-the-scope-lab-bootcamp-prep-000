5930ee3e55281352352d97dff5120c73c1cff14b
Ian Pollack <ianjpollack@gmail.com>
2018-01-10 05:28:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ var funkyFunction = function() {
 // NOTE: you only need to modify the code below this line.
 
 var theFunk = funkyFunction(){
-  function(){
-    function(){
+ return function(){
+    return function(){
       return "FUNKY!"
     }
   }
